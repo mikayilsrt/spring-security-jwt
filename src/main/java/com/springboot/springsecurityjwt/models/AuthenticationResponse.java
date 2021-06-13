d30d1684b7ae05fc7ef91718dde89963d38ac3af
mikayilsrt <sert.mikayil@gmail.com>
2021-06-13 23:10:52 +0200

    first commit

---
+++ src/main/java/com/springboot/springsecurityjwt/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.springboot.springsecurityjwt.models;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return this.jwt;
+    }
+}
